3febc8a5e94c8d61c62ce95d6d70f768075caca8
Kyle DeRusha <cloverindustrys@gmail.com>
2023-01-09 11:36:34 -0500

    Reviews Project Finished

--- tours/App.js
+++ tours/App.js
@@ -10,30 +10,49 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    ;
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
 
     try {
-    const response = await fetch(url);
-    const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
-  }  catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
+      const response = await fetch(url);
+      const tours = await response.json();
+        setLoading(false);
+        setTours(tours);
+    }  catch (error) {
+        setLoading(false);
+       console.log(error);
+      }
+    };
   useEffect(() => {
     fetchTours();
   },[]);
-  if(loading) {
+  if (loading) {
     return <main>
       <Loading />
     </main>
   }
-  return <main>
-    <Tours />
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>No Tours Left</h2>
+          <button className='bin' onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    );
+  }
+ return (
+  <main>
+    <Tours tours={tours} removeTour={removeTour} />
   </main>
-};
+ );
+}
+
 
 export default App
