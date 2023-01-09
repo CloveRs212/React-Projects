29ec840ff8dad21d49a21e0070a35ec112fc08ca
Kyle DeRusha <cloverindustrys@gmail.com>
2023-01-09 12:04:01 -0500

    Accordion Project Done

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -2,6 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
+  const [questions, setQuestions] = setState(data);
+  return <main>
+    <div className='container'>
+      <h3> Questions and Answers About Login</h3>
+      <section className='info'>
+        {
+          questions.map((question) => {
+            return <SingleQuestion key ={question.id} {... question} />
+          })
+        };
+      </section>
+    </div>
+  </main>
   return <h2>accordion project setup</h2>;
 }
 
