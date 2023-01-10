52a235a31365f320bbd638cc2dc428dec01e95f5
Kyle DeRusha <cloverindustrys@gmail.com>
2023-01-10 10:25:13 -0500

    Tabs Project Added

--- menu/src/App.js
+++ menu/src/App.js
@@ -3,8 +3,31 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories =['all',...new Set (items.map((item) => item.category))];
+console.log(allCategories);
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main className='menu-section'>
+      <div className='title'>
+        <h2>Our Menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories filterItems={filterItems} />
+      <Menu Items={menuItems} />
+    </main>
+  );
 }
 
 export default App;
