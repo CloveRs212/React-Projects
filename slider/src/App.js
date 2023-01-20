443a4a72449f6b12b792f9e0abfcdeab624b9143
Kyle DeRusha <cloverindustrys@gmail.com>
2023-01-20 09:36:23 -0500

    Slider Project

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,8 +2,47 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(2);
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const {id, image, title, quote} = person;
+          // More Stuff Coming
+          let position = 'nextSlide';
+          if(personIndex === index) {
+            position = 'activeSlide';
+          }
+          if(personIndex === index - 1) {
+            position = 'lastIndex';
+          }
+          return (
+            <article classname={position} key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
