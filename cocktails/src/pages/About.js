3150c7fd658cd877cb27e9579dbdb00e44c42032
unknown <cloverindustrys@gmail.com>
2023-02-08 23:38:54 -0500

    P15 - Cocktails Started Day 1 Update

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem, this is just random text that i put in here.
+        eos dolorubys
+        animi partial dignlsasdpo asdjklenqrx asdlfd asd.
+        Lorem, this is just random text that i put in here.
+        eos dolorubys
+        animi partial dignlsasdpo asdjklenqrx asdlfd asd.
+        Lorem, this is just random text that i put in here.
+        eos dolorubys
+        animi partial dignlsasdpo asdjklenqrx asdlfd asd.
+      </p>
+    </section>
+  )
+}
+
+export default About
