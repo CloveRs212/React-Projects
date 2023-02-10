8be5942e480689ca94b784f3202a849cbb3c0d3b
unknown <cloverindustrys@gmail.com>
2023-02-10 01:25:31 -0500

    Project 15 Finished, As well as This Repo!

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -4,10 +4,26 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
   const [setSearchTerm] = useGlobalContext()
   const searchValue = React.useRef('');
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+    return (
+    <section className='section-search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>Search Your Favorite Cocktail</label>
+          <input type='text' id='name' ref={searchValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
   )
 }
 
